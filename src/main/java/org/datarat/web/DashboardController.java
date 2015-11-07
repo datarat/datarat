691cc771291a8a5e70fb4c56ec761def4e6f73a9
Bob Naugle <robertjnaugle@gmail.com>
2015-11-06 19:48:09 -0500

    Hey look, Thymeleaf views work, sort of.

--- src/main/java/org/datarat/web/DashboardController.java
+++ src/main/java/org/datarat/web/DashboardController.java
@@ -1,23 +1,22 @@
 package org.datarat.web;
 
-import org.datarat.domain.Dashboard;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author nauglrj1
  */
-@RestController
+@Controller
 public class DashboardController {
     
     @RequestMapping("/")
-    public Dashboard root() {
-        return dashboard();
+    public String root() {
+        return "/dashboard";
     }
     
     @RequestMapping("/dashboard")
-    public Dashboard dashboard() {
-        return new Dashboard();
+    public String dashboard() {
+        return "dashboard";
     }
 }
